ebea86f1d0fe1411c30349e76db215e47b9c765d
hato6502 <erisuke12345@yahoo.co.jp>
2019-04-09 23:47:56 +0900

    静的サイトサーバーの構築に成功しました。

--- render.sh
+++ render.sh
@@ -1,9 +1,6 @@
 #/bin/bash
 
-trap 'kill 0' 1 2 3
-
 cd apps/
-./serve.sh &
 npm run production
 php artisan config:cache
 php artisan route:clear
@@ -11,10 +8,16 @@ php artisan view:clear
 php artisan cache:clear
 
 cd ../
-rm -rf apps.bluehood.local/.
+rm -rf apps.bluehood.local/
 wget --mirror --page-requisites --html-extension http://apps.bluehood.local
 # ここで sitemap.xml も wget すること。つまり、ルートのリストが必要。
+
+for path in `find apps.bluehood.local/ -name '*.html?*'`
+do
+  rename="${path//index.html?/_}"
+  rename="${rename//.html?/.html_}"
+  mv "${path}" "${rename}"
+done
+
 cp -r apps/public/. apps.bluehood.local/
 rm apps.bluehood.local/index.php
-
-kill 0
