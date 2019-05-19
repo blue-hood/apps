04e3382c5f6376513400bbaaa78e5c2c2c3018bf
hato6502 <erisuke12345@yahoo.co.jp>
2019-05-19 20:03:15 +0900

    サイト移行。

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --name bluehood-apps -p 80:80 -v `pwd`/html:/usr/share/nginx/html --restart=always -d static
+docker run --name hood-apps -p 80:80 -v `pwd`/html:/usr/share/nginx/html --restart=always -d static
