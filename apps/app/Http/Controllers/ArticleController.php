2778eb02c1043627ebe2b983541ce55046f3070e
Hata <hato6502@gmail.com>
2019-09-16 00:06:07 +0900

    記事を git 管理するために既存の記事編集機能を削除しました。

--- apps/app/Http/Controllers/ArticleController.php
+++ apps/app/Http/Controllers/ArticleController.php
@@ -21,27 +21,6 @@ class ArticleController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return $this->edit();
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        return $this->update($request);
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param  \App\Article  $article
@@ -52,70 +31,5 @@ class ArticleController extends Controller
         $article->date = Carbon::createFromFormat('Y-m-d H:i:s', $article->updated_at)->format('Y-m-d');
         $article->parsed_content = (new Markdown())->parse($article->content);
         return view('articles.show', compact('article'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Article  $article
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Article $article = null)
-    {
-        $action = is_null($article) ? route('articles.store') : route('articles.update', $article->id);
-        $method = is_null($article) ? 'POST' : 'PUT';
-
-        if (is_null($article)) {
-            $article = new Article();
-        }
-
-        return view('articles.edit', compact('action', 'method', 'article'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Article  $article
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Article $article = null)
-    {
-        $validationList = [
-            'id' => 'required',
-            'thumbnail' => 'required',
-            'title' => 'required',
-            'description' => 'required',
-            'content' => 'required'
-        ];
-        if (is_null($article)) {
-            $validationList['id'] .= '|unique:articles';
-        }
-        $request->validate($validationList);
-
-        $newArticle = Article::firstOrNew(['id' => $request->id]);
-        $newArticle->thumbnail = $request->thumbnail;
-        $newArticle->title = $request->title;
-        $newArticle->description = $request->description;
-        $newArticle->content = $request->content;
-        $newArticle->save();
-
-        if (!is_null($article) && $article->id !== $newArticle->id) {
-            $article->delete();
-        }
-
-        return redirect(route('articles.show', $newArticle->id));
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Article  $article
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Article $article)
-    {
-        $article->delete();
-        return redirect(route('articles.index'));
     }
 }
